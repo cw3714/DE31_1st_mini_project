d11524f9a695df681e28f93bc1df6ce4148ad01c
S0rrow <jae9hell@gmail.com>
2024-04-18 15:29:17 +0900

    PRESET updated; code snippets cleaning on progress

---
+++ connect_to_db.py
@@ -0,0 +1,32 @@
+# import
+import pandas as pd
+import numpy as np
+import selenium
+import sqlalchemy
+from urllib import parse
+import requests
+from bs4 import BeautifulSoup as BS
+import os
+import json
+from tqdm import tqdm
+import pickle
+from geopy.geocoders import Nominatim
+import subprocess
+
+# custom python lib
+import evchargestations as EV
+
+metro_codes = EV.metro_codes
+
+with open("db_info.json", "r") as f:
+    db_info = json.load(f)
+    
+def connect_to_db():
+    user = db_info['user']
+    password = db_info['password']
+    host = db_info['host']
+    port = db_info['port']
+    database = db_info['database']
+    password = parse.quote_plus(password)
+    engine = sqlalchemy.create_engine(f"mysql://{user}:{password}@{host}:{port}/{database}?charset=utf8mb4")
+
